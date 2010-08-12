b7c1c2df8c4ff17360b43f6682e4aaefe6e85e0b
Markus Barenhoff <alios@alios.org>
2010-08-12 10:16:59 +0200

    splittet generic stuff out to Nmea0183

--- Parser.hs
+++ Parser.hs
@@ -1,45 +1,13 @@
-{-# OPTIONS -fglasgow-exts #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+module Parser () where
 
-
-module GisServer.Nmea0183.Parser () where
-
-import Data.Bits
-import Data.Word
-import Data.Char
-import Data.ByteString.Internal(c2w, w2c)
 import Data.Generics(Data)
 import Data.Typeable    
 import Data.Binary
 import Data.Binary.Get
 import Control.Applicative
 
-isNmeaChar :: Word8 -> Bool
-isNmeaChar c = (c .&. 0x80) == 0x00
-
-startChar = c2w '$'
-isStartChar = (==) startChar
-
-
-reservedChars :: [ Word8 ]
-reservedChars = map c2w "$*,!\\^~" ++ [ 0x0d, 0x0a ]
-isReservedChar c = elem c reservedChars
-
-validChars = [ c | c <- [0x20 .. 0x7e], not $ isReservedChar c]
-isValidChar c = elem c validChars
-
--- TODO: add test case that all valid and Reserved chars are transmittable
-
-
-undefChars = [ c | c <- [0x00 .. 0xff]
-             , isNmeaChar c
-             , not $ isReservedChar c
-             , not $ isValidChar c]
-
-isUndefChar c = elem c undefChars
-
-transmittableChars = [ c | c <- reservedChars ++ validChars
-                     , isNmeaChar c]
-isTransmittableChar c = elem c transmittableChars
+import Nmea0183 
 
 getTransmittableChar :: Get Word8
 getTransmittableChar = 
@@ -49,9 +17,6 @@ getTransmittableChar =
            fail $ show c ++ " is not a transmittable char."
                   
 
-addrFieldChars = [ c | c <- validChars
-                 , (isUpper $ w2c c) || (isDigit $ w2c c)]
-isAddrFieldChar c = elem c addrFieldChars
 
 data AddressField = 
     AAF { 
@@ -63,16 +28,3 @@ data AddressField =
     } 
     deriving (Read, Show, Eq, Data, Typeable)
                   
-
-
---x = lookAheadM . optional   
-
-{-
-skipUntil :: Get a -> Get a
-skipUntil p = do
-  r <- lookAheadM p
-  case r of
-    Just c  -> return c
-    Nothing -> skipUntil p
-
--}
