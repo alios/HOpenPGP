84edbbd7fda870596c1515e66aea42549d6f9705
Markus Barenhoff <alios@alios.org>
2010-09-01 12:03:23 +0200

    added great circle distance calculations

--- Geodatic.hs
+++ Geodatic.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE GADTs, EmptyDataDecls, FlexibleInstances, Rank2Types, FlexibleContexts, TypeSynonymInstances, LiberalTypeSynonyms #-}
 
-module Geodatic ( ans, grs80, wgs84 
+module Geodatic ( GeodaticModelT(..)
+                , ans, grs80, wgs84, ans', grs80', wgs84' 
                 , toEcef, toGeodatic ) where
 
 import qualified Prelude
@@ -38,39 +39,46 @@ class (Eq m, Show m) => GeodaticModel m where
         b = ((_1 - f) ** _2)
     in  a / b
        
-data Geodatic
-data ECEF
-
 data Coordinate t where
-  GeodaticCoordinate :: (Eq t, Show t, Num t, RealFloat t) => GeodaticModelT -> 
+  GeodaticCoordinate :: (Eq t, Show t, Num t, RealFloat t) => 
+                        GeodaticModelT -> 
                         PlaneAngle t-> 
                         PlaneAngle t -> 
                         Length t -> 
-                        Coordinate Geodatic
-  ECEFCoordinate :: (Eq t, Show t, Num t, RealFloat t) =>Length t -> Length t -> Length t -> Coordinate ECEF
+                        Coordinate t
+  ECEFCoordinate :: (Eq t, Show t, Num t, RealFloat t) =>Length t -> Length t -> Length t -> Coordinate t
   
-{- instance Eq (Coordinate t) where
+
+
+
+instance Eq (Coordinate t) where
   (ECEFCoordinate x y z) == (ECEFCoordinate x' y' z') = (x == x') &&
                                                         (y == y') && 
                                                         (z == z')
+  a@(GeodaticCoordinate m φ λ h) == b@(GeodaticCoordinate m' φ' λ' h') =
+    if (m == m') then (φ == φ') && (λ == λ') && (h == h) else
+      toGeodatic m b == a
+  a == b = toEcef a == toEcef b
+
+--instance Num (Coordinate t) where
   
-  -}
 
 instance Show (Coordinate t) where
   show (GeodaticCoordinate m φ λ h) = show (m,φ,λ,h)
   show (ECEFCoordinate x y z) = show (x,y,z)
   
-ans φ λ h = 
-  GeodaticCoordinate ANS (φ *~ degree) (λ *~ degree) (h *~ meter)
-
-grs80 φ λ h = 
-  GeodaticCoordinate GRS80 (φ *~ degree) (λ *~ degree) (h *~ meter)
-
-wgs84 φ λ h = 
-  GeodaticCoordinate WGS84 (φ *~ degree) (λ *~ degree) (h *~ meter)
 
 
-toEcef :: Coordinate t -> Coordinate ECEF
+ans' φ λ h= GeodaticCoordinate ANS φ λ h
+ans φ λ h = ans' (φ *~ degree) (λ *~ degree) (h *~ meter)
+
+grs80' φ λ h = GeodaticCoordinate GRS80 φ λ h
+grs80 φ λ h = grs80' (φ *~ degree) (λ *~ degree) (h *~ meter)
+
+wgs84' φ λ h = GeodaticCoordinate WGS84 φ λ h
+wgs84 φ λ h = wgs84'(φ *~ degree) (λ *~ degree) (h *~ meter)
+
+toEcef :: Coordinate t -> Coordinate t
 toEcef c@(ECEFCoordinate _ _ _) = c
 toEcef (GeodaticCoordinate m φ λ h) =
   let e2 = fstEccentricity m
@@ -83,14 +91,14 @@ toEcef (GeodaticCoordinate m φ λ h) =
       rz = (((a * ( _1 - e2)) / x) + h) * (sin φ)
   in ECEFCoordinate rx ry rz
      
-toGeodatic :: Coordinate t -> GeodaticModelT -> Coordinate Geodatic
-toGeodatic c@(GeodaticCoordinate m φ λ h) tm =
-  if (tm == m) then c else toGeodatic (toEcef c) tm
-toGeodatic c@(ECEFCoordinate x y z) m = 
-  let a = semiMajorAxis m
-      b = semiMinorAxis m
-      e2 = fstEccentricity m
-      e'2 = sndEccentricity m
+toGeodatic :: GeodaticModelT -> Coordinate t -> Coordinate t
+toGeodatic tm c@(GeodaticCoordinate m φ λ h) =
+  if (tm == m) then c else toGeodatic tm (toEcef c)
+toGeodatic tm c@(ECEFCoordinate x y z) = 
+  let a = semiMajorAxis tm
+      b = semiMinorAxis tm
+      e2 = fstEccentricity tm
+      e'2 = sndEccentricity tm
       r = sqrt ((x * x) + (y * y))
       ee2 = (a * a) - (b * b)
       f = (54 *~ one) * (b * b) * (z * z)
@@ -112,12 +120,33 @@ toGeodatic c@(ECEFCoordinate x y z) m =
       h = u * (_1 - ((b * b)/(a * v)))
       φ = atan ((z + (e'2 * z0)) / r)
       λ = atan2 y x
-  in GeodaticCoordinate m φ λ h
-     
-     
-ms = wgs84 57.6 7.42 35     
-mse = toEcef ms
-msa1 = toGeodatic ms ANS
-msa2 = toGeodatic mse ANS
-msb = toGeodatic mse WGS84
+  in GeodaticCoordinate tm φ λ h
+
+
+gcDist :: Coordinate t -> Coordinate t -> Length t
+gcDist a@(ECEFCoordinate _ _ _) b@(ECEFCoordinate _ _ _) =
+  gcDist (toGeodatic WGS84 a) (toGeodatic WGS84 b)
+gcDist a@(GeodaticCoordinate m _ _ _) b@(ECEFCoordinate _ _ _) =
+  gcDist a (toGeodatic m b)
+gcDist a@(ECEFCoordinate _ _ _) b@(GeodaticCoordinate m _ _ _) =
+  gcDist (toGeodatic m a) b
+gcDist c1@(GeodaticCoordinate m1 φ1 λ1 __) c2@(GeodaticCoordinate m2 φ2 λ2 _)
+  | (m1 /= m2) = gcDist c1 (toGeodatic m1 c2)
+  | otherwise = 
+      let dλ = abs $ λ1 - λ2
+          a1 = (cos φ1 * sin dλ)
+          a2 = (cos φ2 * sin φ1) - (sin φ2 * cos φ1 * cos dλ)
+          a = sqrt ((a1 * a1) + (a2 *a2))     
+          b = (sin φ2 * sin φ1) + (cos φ2 * cos φ1 * cos dλ)      
+          dσ = atan (a / b)
+          r = 6371.01 *~ kilo meter
+      in r * dσ
+  
+
+
+ms = wgs84 51.969659 7.605286 0
+hh = wgs84 53.543572 10.02502 0
+d1 = wgs84 36.12 (-86.67) 0
+d1a = ans 36.12 (-86.67) 0
+d2 = wgs84 33.94 (-118.40) 0
 
