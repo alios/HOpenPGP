449d37294f97fb49cfd927681116452f2b6748fa
Markus Barenhoff <mbarenh@alios.org>
2011-01-05 20:19:43 +0100

    no longer line buffered

--- rot13.hs
+++ rot13.hs
@@ -1,23 +1,26 @@
+module Main (main, rot13) where
 
-module Main(main) where
-
-
-
-rot l =
-  let a1 = cycle l
-      a2 = drop 13 a1
-  in take 26 $ zip a1 a2
+import System.IO
 
 chars = rot ['A'..'Z'] ++ rot ['a'..'z']
 
+rot13 :: Char -> Char
 rot13 c =
   let r = lookup c chars
   in case r of
     Nothing -> c
     Just c' -> c'
     
+main :: IO ()
+main = do
+  eof <- hIsEOF stdin
+  if (eof) then return ()
+    else do c <- getChar
+            putChar $ rot13 c
+            main
 
 
-rot13str = map rot13
-
-main = interact rot13str
+rot l =
+  let a1 = cycle l
+      a2 = drop 13 a1
+  in take 26 $ zip a1 a2
